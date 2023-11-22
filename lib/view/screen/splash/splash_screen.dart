26b589b06452c91fe63f3b0b9f482312e091b5d3
Darshan <darshan.welinfoweb@gmail.com>
2023-11-22 17:00:59 +0530

    Initial Commit

--- lib/view/screen/splash/splash_screen.dart
+++ lib/view/screen/splash/splash_screen.dart
@@ -34,7 +34,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    permissionServiceCall();
+    // permissionServiceCall();
     print("internetStatus:::::1:::$isNotConnected");
     AppConstants.closeKeyboard();
     dbManager.deleteAll();
